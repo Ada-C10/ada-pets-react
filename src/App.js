d92aab5bc4f2f7cfbe7ac4bdefaa82761612b491
dee <dee@adadevelopersacademy.org>
2018-12-12 11:54:16 -0800

    convert petlist to classical comp, remove hardcoded pets json data

--- src/App.js
+++ src/App.js
@@ -7,27 +7,17 @@ import NewPetForm from './components/NewPetForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import pets from './data/pets.json';
-
 class App extends Component {
   constructor(props) {
     super(props);
-    const petList = pets.map((pet) => {
-      pet.currentPet = 0;
-      pet.images = pet.images.map((filename) => {
-        return `/images/${filename}`;
-      });
-      return pet;
-    });
 
     this.state = {
-      petList,
       currentPet: undefined,
     };
   }
 
   onSelectPet = (petId) => {
-    
+
     const selectedPet = this.state.petList.find((pet) => {
       return pet.id === petId;
     });
@@ -38,45 +28,10 @@ class App extends Component {
     }
   }
 
-  onSearchChange = (value) => {
-    console.log(value);
-    const regex = new RegExp(`${value}`.toUpperCase());
-    const petList = pets.filter((pet) => {
-      return regex.test(`${pet.name}${pet.about}${pet.species}`.toUpperCase());
-    });
-
-    this.setState({
-      petList,
-    });
-  }
-
-  addPet = (newPet) => {
-    newPet.id = pets.reduce((max = 0, currentPet) => max ? Math.max(max, currentPet.id): currentPet.id) + 1
-    pets.push(newPet);
-    this.setState({
-      petList: pets,
-    });
-  }
-
-  removePet = (petId) => {
-    let deleteIndex = -1;
-    pets.forEach((pet, index) => {
-      if (petId === pet.id) {
-        deleteIndex = index;
-      }
-    });
-    
-    pets.splice(deleteIndex, 1);
-
-    this.setState({
-      petList: pets,
-    })
-  }
-
   render() {
     const { currentPet } = this.state;
     console.log(this.state.petList);
-    
+
     const details = currentPet ? <PetDetails currentPet={currentPet} /> : '';
 
     return (
@@ -84,19 +39,9 @@ class App extends Component {
         <header className="app-header">
           <h1>Ada Pets</h1>
         </header>
-        <section className="search-bar">
-          <SearchBar onSearchChange={this.onSearchChange} />
-        </section>
-         {details}
         <section className="pet-list">
-          <PetList 
-            selectPetCallback={this.onSelectPet} 
-            deletePetCallback={this.removePet}
-            pets={this.state.petList} 
+          <PetList
           />
-        </section>
-        <section>
-          <NewPetForm addPetCallback={this.addPet} />
         </section>
       </main>
     );
--- src/components/PetList.js
+++ src/components/PetList.js
@@ -1,28 +1,32 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import PetCard from './PetCard';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const PetList = (props) => {
-  const petList = props.pets.map((pet) => {
-    return <PetCard key={pet.id} 
-             deletePetCallback={props.deletePetCallback}
-             selectPetCallback={props.selectPetCallback} 
-             {...pet} />
-  });
+class PetList extends React.Component {
 
-  return (
-    <div className="card-group">
-      {petList}
-    </div>
-  )
+  constructor(props){
+    super(props);
+
+    this.state = {
+      pets: [],
+    }
+  }
+
+  render() {
+    const petList = this.state.pets.map((pet) => {
+      return <PetCard key={pet.id}
+               {...pet} />
+    });
+
+    return (
+      <div className="card-group">
+        {petList}
+      </div>
+    )
+  }
 }
 
-PetList.propTypes = {
-  pets: PropTypes.array.isRequired,
-  onSelectPet: PropTypes.func,
-};
 
 export default PetList;
